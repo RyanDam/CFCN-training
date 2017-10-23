aba4af5325db4d789ef68236bdc88a73d2db5b5c
ryan <py.ryan.dam@gmail.com>
2017-10-23 12:44:45 +0700

    edit file path

--- data/layers/nifti_to_npy.py
+++ data/layers/nifti_to_npy.py
@@ -20,28 +20,28 @@ import nibabel
 ##### 3DIRCA DATASET ######
 ###########################
 
-IRCA_NUMPY_BASE_PATH = '../../3Dircadb1/niftis_segmented_all/'
+IRCA_NUMPY_BASE_PATH = '/media/sivic/DATA/Duydam/saratan/3Dircadb1/niftis_segmented_all/'
 irca_numpy_all = [\
 (301,IRCA_NUMPY_BASE_PATH+"image01.nii",IRCA_NUMPY_BASE_PATH+"label01.nii"),
 (302,IRCA_NUMPY_BASE_PATH+"image02.nii",IRCA_NUMPY_BASE_PATH+"label02.nii"),
 (303,IRCA_NUMPY_BASE_PATH+"image03.nii",IRCA_NUMPY_BASE_PATH+"label03.nii"),
 (304,IRCA_NUMPY_BASE_PATH+"image04.nii",IRCA_NUMPY_BASE_PATH+"label04.nii"),
-#(305,IRCA_NUMPY_BASE_PATH+"image05.nii",IRCA_NUMPY_BASE_PATH+"label05.nii"),
+(305,IRCA_NUMPY_BASE_PATH+"image05.nii",IRCA_NUMPY_BASE_PATH+"label05.nii"),
 (306,IRCA_NUMPY_BASE_PATH+"image06.nii",IRCA_NUMPY_BASE_PATH+"label06.nii"),
-#(307,IRCA_NUMPY_BASE_PATH+"image07.nii",IRCA_NUMPY_BASE_PATH+"label07.nii"),
+(307,IRCA_NUMPY_BASE_PATH+"image07.nii",IRCA_NUMPY_BASE_PATH+"label07.nii"),
 (308,IRCA_NUMPY_BASE_PATH+"image08.nii",IRCA_NUMPY_BASE_PATH+"label08.nii"),
 (309,IRCA_NUMPY_BASE_PATH+"image09.nii",IRCA_NUMPY_BASE_PATH+"label09.nii"),
 (310,IRCA_NUMPY_BASE_PATH+"image10.nii",IRCA_NUMPY_BASE_PATH+"label10.nii"),
-#(311,IRCA_NUMPY_BASE_PATH+"image11.nii",IRCA_NUMPY_BASE_PATH+"label11.nii"),
+(311,IRCA_NUMPY_BASE_PATH+"image11.nii",IRCA_NUMPY_BASE_PATH+"label11.nii"),
 (312,IRCA_NUMPY_BASE_PATH+"image12.nii",IRCA_NUMPY_BASE_PATH+"label12.nii"),
 (313,IRCA_NUMPY_BASE_PATH+"image13.nii",IRCA_NUMPY_BASE_PATH+"label13.nii"),
-#(314,IRCA_NUMPY_BASE_PATH+"image14.nii",IRCA_NUMPY_BASE_PATH+"label14.nii"),
+(314,IRCA_NUMPY_BASE_PATH+"image14.nii",IRCA_NUMPY_BASE_PATH+"label14.nii"),
 (315,IRCA_NUMPY_BASE_PATH+"image15.nii",IRCA_NUMPY_BASE_PATH+"label15.nii"),
 (316,IRCA_NUMPY_BASE_PATH+"image16.nii",IRCA_NUMPY_BASE_PATH+"label16.nii"),
 (317,IRCA_NUMPY_BASE_PATH+"image17.nii",IRCA_NUMPY_BASE_PATH+"label17.nii"),
 (318,IRCA_NUMPY_BASE_PATH+"image18.nii",IRCA_NUMPY_BASE_PATH+"label18.nii"),
-(319,IRCA_NUMPY_BASE_PATH+"image19.nii",IRCA_NUMPY_BASE_PATH+"label19.nii")]
-#(320,IRCA_NUMPY_BASE_PATH+"image20.nii",IRCA_NUMPY_BASE_PATH+"label20.nii")]
+(319,IRCA_NUMPY_BASE_PATH+"image19.nii",IRCA_NUMPY_BASE_PATH+"label19.nii"),
+(320,IRCA_NUMPY_BASE_PATH+"image20.nii",IRCA_NUMPY_BASE_PATH+"label20.nii")]
 
 if __name__ == '__main__':	
 
