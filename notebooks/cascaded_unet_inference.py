aebceddf34f339da087182ee9901d2390d57d937
RyanDam <py.ryan.dam@gmail.com>
2017-11-03 13:26:10 +0700

    test retrain step 1

--- notebooks/cascaded_unet_inference.py
+++ notebooks/cascaded_unet_inference.py
@@ -27,10 +27,11 @@
 STEP1_DEPLOY_PROTOTXT = "inference/step1_deploy.prototxt"
 # STEP1_MODEL_WEIGHTS   = "inference/step1_weights.caffemodel"
 # STEP1_MODEL_WEIGHTS   = "/mnt/data/student/snapshot_save/snapshot_step1_retrain/_iter_38000.caffemodel"
-STEP1_MODEL_WEIGHTS   = "/mnt/data/student/snapshot_step1/_iter_2500.caffemodel"
+# STEP1_MODEL_WEIGHTS   = "/mnt/data/student/snapshot_step1/_iter_2500.caffemodel"
+STEP1_MODEL_WEIGHTS   = "/mnt/data/student/snapshot/_iter_8500.caffemodel"
 STEP2_DEPLOY_PROTOTXT = "inference/step2_deploy.prototxt"
 # STEP2_MODEL_WEIGHTS   = "inference/step2_weights.caffemodel"
-STEP2_MODEL_WEIGHTS   = "/mnt/data/student/snapshot/_iter_23000.caffemodel"
+STEP2_MODEL_WEIGHTS   = "/mnt/data/student/snapshot_step2/_iter_23000.caffemodel"
 
 PATIENT_DICOM_PATH = "test_image/3Dircadb1.19/PATIENT_DICOM/"
 PATIENT_MASH_PATH = "test_image/3Dircadb1.19/MASKS_DICOM/"
