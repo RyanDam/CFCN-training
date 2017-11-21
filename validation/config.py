9c175b8414754066d1c7f113f09314055ed5582e
RyanDam <py.ryan.dam@gmail.com>
2017-11-21 09:44:02 +0700

    change prototxt

--- validation/config.py
+++ validation/config.py
@@ -58,7 +58,7 @@ dataset = irca_all[10:]
 models = ['/mnt/data/student/snapshot_step1_3input_enhance/_iter_17500.caffemodel']*5
 models_step_two = ['/mnt/data/student/snapshot_step2_3input/_iter_62000.caffemodel']*5
 
-deployprototxt = ['/mnt/data/student/deploy/step1_deploy.prototxt']*5
-deployprototxt_step_two = ['/mnt/data/student/deploy/step2_deploy.prototxt']*5
+deployprototxt = ['/mnt/data/student/deploy/step1_deploy_3input.prototxt']*5
+deployprototxt_step_two = ['/mnt/data/student/deploy/step2_deploy_3input.prototxt']*5
 
 
