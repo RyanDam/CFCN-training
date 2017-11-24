3ef4a645b0c3fc4ba48e87ab067b1cc59a3e4ddf
RyanDam <py.ryan.dam@gmail.com>
2017-11-24 22:13:53 +0700

    no message

--- validation/config.py
+++ validation/config.py
@@ -31,31 +31,34 @@ irca_all= [\
 (302,IRCA_BASE_PATH+"image02.nii",IRCA_BASE_PATH+"label02.nii",[0.78,0.78,1.6]),
 (303,IRCA_BASE_PATH+"image03.nii",IRCA_BASE_PATH+"label03.nii",[0.62,0.62,1.25]),
 (304,IRCA_BASE_PATH+"image04.nii",IRCA_BASE_PATH+"label04.nii",[0.74,0.74,2.]),
-#(305,IRCA_BASE_PATH+"image05.nii",IRCA_BASE_PATH+"label05.nii",[0.78,0.78,1.6]),
+(305,IRCA_BASE_PATH+"image05.nii",IRCA_BASE_PATH+"label05.nii",[0.78,0.78,1.6]),#
 (306,IRCA_BASE_PATH+"image06.nii",IRCA_BASE_PATH+"label06.nii",[0.78,0.78,1.6]),
-#(307,IRCA_BASE_PATH+"image07.nii",IRCA_BASE_PATH+"label07.nii",[0.78,0.78,1.6]),
+(307,IRCA_BASE_PATH+"image07.nii",IRCA_BASE_PATH+"label07.nii",[0.78,0.78,1.6]),#
 (308,IRCA_BASE_PATH+"image08.nii",IRCA_BASE_PATH+"label08.nii",[0.56,0.56,1.6]),
 (309,IRCA_BASE_PATH+"image09.nii",IRCA_BASE_PATH+"label09.nii",[0.87,0.87,2.]),
 (310,IRCA_BASE_PATH+"image10.nii",IRCA_BASE_PATH+"label10.nii",[0.73,0.73,1.6]),
-#(311,IRCA_BASE_PATH+"image11.nii",IRCA_BASE_PATH+"label11.nii",[0.72,0.72,1.6]),
+(311,IRCA_BASE_PATH+"image11.nii",IRCA_BASE_PATH+"label11.nii",[0.72,0.72,1.6]),#
 (312,IRCA_BASE_PATH+"image12.nii",IRCA_BASE_PATH+"label12.nii",[0.68,0.68,1.]),
 (313,IRCA_BASE_PATH+"image13.nii",IRCA_BASE_PATH+"label13.nii",[0.67,0.67,1.6]),
-#(314,IRCA_BASE_PATH+"image14.nii",IRCA_BASE_PATH+"label14.nii",[0.72,0.72,1.6]),
+(314,IRCA_BASE_PATH+"image14.nii",IRCA_BASE_PATH+"label14.nii",[0.72,0.72,1.6]),#
 (315,IRCA_BASE_PATH+"image15.nii",IRCA_BASE_PATH+"label15.nii",[0.78,0.78,1.6]),
 (316,IRCA_BASE_PATH+"image16.nii",IRCA_BASE_PATH+"label16.nii",[0.7,0.7,1.6]),
 (317,IRCA_BASE_PATH+"image17.nii",IRCA_BASE_PATH+"label17.nii",[0.74,0.74,1.6]),
 (318,IRCA_BASE_PATH+"image18.nii",IRCA_BASE_PATH+"label18.nii",[0.74,0.74,2.5]),
-(319,IRCA_BASE_PATH+"image19.nii",IRCA_BASE_PATH+"label19.nii",[0.7,0.7,4.])]
-#(320,IRCA_BASE_PATH+"image20.nii",IRCA_BASE_PATH+"label20.nii",[0.81,0.81,2.])]
+(319,IRCA_BASE_PATH+"image19.nii",IRCA_BASE_PATH+"label19.nii",[0.7,0.7,4.]),
+(320,IRCA_BASE_PATH+"image20.nii",IRCA_BASE_PATH+"label20.nii",[0.81,0.81,2.])]#
 
 #Datset to test
-dataset = irca_all[10:]
+dataset = irca_all[:]
 
 #Paths to models. first element corresponds to first fold, second element to second fold etc.
-models = ['/mnt/data/student/deploy/our/step1_weights.caffemodel']*5
-models_step_two = ['/mnt/data/student/deploy/our/step2_weights.caffemodel']*5
+# models = ['/mnt/data/student/deploy/our/step1_weights.caffemodel']*5
+# models_step_two = ['/mnt/data/student/deploy/our/step2_weights.caffemodel']*5
 
-deployprototxt = ['/mnt/data/student/deploy/step1_deploy.prototxt']*5
-deployprototxt_step_two = ['/mnt/data/student/deploy/step2_deploy.prototxt']*5
+models = ['/mnt/data/student/snapshot_step1_3input_enhance2/_iter_1000.caffemodel']*20
+models_step_two = ['/mnt/data/student/snapshot_step2_3input/_iter_62000.caffemodel']*20
+
+deployprototxt = ['/mnt/data/student/deploy/step1_deploy_3input.prototxt']*20
+deployprototxt_step_two = ['/mnt/data/student/deploy/step2_deploy_3input.prototxt']*20
 
 
