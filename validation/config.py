31aa9c8ba9a03a7eb48152c7f8bb7f42dd16c29e
RyanDam <py.ryan.dam@gmail.com>
2017-12-10 20:29:15 +0700

    no message

--- validation/config.py
+++ validation/config.py
@@ -52,10 +52,10 @@ irca_all= [\
 dataset = irca_all[:]
 
 #Paths to models. first element corresponds to first fold, second element to second fold etc.
-# models = ['/mnt/data/student/deploy/our/step1_weights.caffemodel']*20
-models_step_two = ['/mnt/data/student/deploy/our/step2_weights.caffemodel']*20
-models = ['/mnt/data/student/snapshot_step1_p1_2/_iter_7000.caffemodel']*20
-# models_step_two = ['/mnt/data/student/snapshot_step2_1input/_iter_44000.caffemodel']*20
+models = ['/mnt/data/student/deploy/our/step1_weights.caffemodel']*20
+# models = ['/mnt/data/student/snapshot_step1_p1_2/_iter_7000.caffemodel']*20
+# models_step_two = ['/mnt/data/student/deploy/our/step2_weights.caffemodel']*20
+models_step_two = ['/mnt/data/student/snapshot/_iter_83000.caffemodel']*20
 deployprototxt = ['/mnt/data/student/deploy/step1_deploy.prototxt']*20
 deployprototxt_step_two = ['/mnt/data/student/deploy/step2_deploy.prototxt']*20
 
